56811e1551f7a99bde9ea80fc50bacfc51b8cc4a
Ivan Lysov <ivan.lysov@gmail.com>
2024-12-03 17:19:25 +0500

    print

--- examples/scenes/payments.gd
+++ examples/scenes/payments.gd
@@ -31,8 +31,10 @@ func _on_purchase_completed(success, purchase):
 	if purchase != null:
 		match Bridge.platform.id:
 			"yandex":
+				print("Purchase ID: " + str(purchase.productID))
 				print("Purchase Token: " + str(purchase.purchaseToken))
 			"facebook":
+				print("Purchase ID: " + str(purchase.productID))
 				print("Purchase Token: " + str(purchase.purchaseToken))
 
 
